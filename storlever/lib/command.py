421bdc5a43d37c9366ad72f368c33061e8bb8686
jamken <zqwangjk@sina.com>
2013-12-25 19:41:12 +0800

    backport to 2.6

--- storlever/lib/command.py
+++ storlever/lib/command.py
@@ -12,6 +12,29 @@ import subprocess
 import os
 import exception
 
+# backport check_output to python 2.6
+if "check_output" not in dir( subprocess ):
+    def backport_check_output(*popenargs, **kwargs):
+        r"""Run command with arguments and return its output as a byte string.
+
+        Backported from Python 2.7 as it's implemented as pure python on stdlib.
+
+        >>> check_output(['/usr/bin/python', '--version'])
+        Python 2.6.2
+        """
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            error = subprocess.CalledProcessError(retcode, cmd)
+            error.output = output
+            raise error
+        return output
+    subprocess.check_output = backport_check_output;
+
 
 def check_output(cmd, shell=False, input_ret=[]):
     """call the cmd and return output
