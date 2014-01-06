30668311eb171c78ee6f590bc03998062e363624
jamken <zqwangjk@sina.com>
2014-01-06 23:25:17 +0800

    add some fs support

--- storlever/mngr/fs/fs.py
+++ storlever/mngr/fs/fs.py
@@ -1,42 +1,80 @@
-class FS(object):
-    def __init__(self):
-        self.name = ''
-        self.size = 0
-        self.avail_size = 0
-        self.free_size = 0
-        self.lv = None
-        self.type = 'ext4'
-        self.stripe_size = 0
+"""
+storlever.mngr.fs.fs
+~~~~~~~~~~~~~~~~
+
+filesystem base class. All filesystem type must inherit this class.
+
+:copyright: (c) 2013 by jk.
+:license: GPLv3, see LICENSE for more details.
+
+"""
+import os
+from storlever.lib.command import check_output
+
+
+PROC_MOUNT_FILE = "/proc/mounts"
+
+
+class FileSystem(object):
+    def __init__(self, name, fs_conf):
+        self.name = name
+        self.fs_conf = fs_conf
 
     @classmethod
-    def mkfs(cls):
-        pass
+    def mkfs(cls, type, dev_file, fs_options=""):
+        cmd = "/sbin/mkfs -t %s %s %s" % (type, fs_options, dev_file)
+        check_output(cmd, shell=True, input_ret=[1])
+
+    @property
+    def mount_options(self):
+        return self.fs_conf["mount_option"]
 
     def mount(self):
+        check_output(["/bin/mount", "-t", self.fs_conf["type"],
+                      "-o", self.mount_options,
+                      self.fs_conf["dev_file"], self.fs_conf["mount_point"]],
+                     input_ret=[32])
+
+    def umount(self):
+        check_output(["/bin/umount", "-f",
+                      self.fs_conf["mount_point"]],
+                     input_ret=[1])
+
+    def is_available(self):
+        with open(PROC_MOUNT_FILE, "r") as f:
+            for line in f:
+                mount_entry = line.split()
+                if mount_entry[0] == self.fs_conf["dev_file"] and \
+                   mount_entry[1] == self.fs_conf["mount_point"]:
+                    return True
+        return False
+
+    def usage_info(self):
+        if self.is_available():
+            st = os.statvfs(self.fs_conf["mount_point"])
+            available = (st.f_bavail * st.f_frsize)
+            total = (st.f_blocks * st.f_frsize)
+            used = (st.f_blocks - st.f_bfree) * st.f_frsize
+            try:
+                percent = (float(used) / total) * 100
+            except ZeroDivisionError:
+                percent = 0
+        else:
+            total = 0
+            available = 0
+            used = 0
+            percent = 0
+
+        return {"total": total, "available": available,
+                "used": used, "percent": round(percent, 1)}
+
+    def fs_meta_dump(self):
+        return ""
+
+    def grow_size(self):
         pass
 
-    def unmount(self):
-        pass
 
 
-class EXT4FS(FS):
-    def __init__(self):
-        super(EXT4FS, self).__init__()
 
 
-class XFS(FS):
-    def __init__(self):
-        super(XFS, self).__init__()
-
-
-class Share(object):
-    def __init__(self):
-        self.name = ''
-        self.fs = None
-
-    @classmethod
-    def new(cls):
-        pass
-
-    def delete(self):
-        pass
