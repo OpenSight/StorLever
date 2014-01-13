51608a6d4bad828b47a5d4ac84d4fad83a92e567
jamken <zqwangjk@sina.com>
2014-01-13 18:07:07 +0800

    add some share directory operation

--- storlever/mngr/fs/fs.py
+++ storlever/mngr/fs/fs.py
@@ -13,6 +13,8 @@ import stat
 from storlever.lib.command import check_output
 from storlever.mngr.system.usermgr import user_mgr
 from storlever.lib.exception import StorLeverError
+from storlever.lib import logger
+import logging
 
 PROC_MOUNT_FILE = "/proc/mounts"
 
@@ -76,19 +78,18 @@ class FileSystem(object):
     def grow_size(self):
         pass
 
-    def create_share(self, name, user=None, group=None, mode = 0777):
+    def create_share(self, name, user=None, group=None, mode=0777, operator="unknown"):
         # make sure fs is available
         if not self.is_available():
             raise StorLeverError("File system is unavailable", 400)
+        umgr = user_mgr()
         if user is None:
             uid = -1
         else:
-            umgr = user_mgr()
             uid = umgr.get_user_info_by_name(user)["uid"]
         if group is None:
             gid = -1
         else:
-            umgr = user_mgr()
             gid = umgr.get_group_info_by_name(group)["gid"]
         uid = umgr.get_user_info_by_name()
         mount_point = self.fs_conf["mount_point"]
@@ -96,8 +97,25 @@ class FileSystem(object):
         os.mkdir(path, mode)
         os.chown(path, uid, gid)
 
-    def delete_share(self):
-        pass
+        logger.log(logging.INFO, logger.LOG_TYPE_CONFIG,
+                   "Share directory (%s) is created"
+                   " by user(%s)" %
+                   (path, operator))
+
+    def delete_share(self, name, user="unknown"):
+        # make sure fs is available
+        if not self.is_available():
+            raise StorLeverError("File system is unavailable", 400)
+        path = os.path.join(self.fs_conf["mount_point"], name)
+        if not os.path.exists(path):
+            raise StorLeverError("Share directory not found", 404)
+        check_output(["/bin/rm", "-rf", path],
+                     input_ret=[1])
+
+        logger.log(logging.INFO, logger.LOG_TYPE_CONFIG,
+                   "Share directory (%s) is deleted"
+                   " by user(%s)" %
+                   (path, user))
 
     def _get_uid_map(self):
         umgr = user_mgr();
@@ -150,15 +168,40 @@ class FileSystem(object):
 
         return output_list
 
+    def mod_share_owner(self, name, user = None, group = None, operator="unknown"):
+        if not self.is_available():
+            raise StorLeverError("File system is unavailable", 400)
+        path = os.path.join(self.fs_conf["mount_point"], name)
+        if not os.path.exists(path):
+            raise StorLeverError("Share directory not found", 404)
+        umgr = user_mgr()
+        if user is None:
+            uid = -1
+        else:
+            uid = umgr.get_user_info_by_name(user)["uid"]
+        if group is None:
+            gid = -1
+        else:
+            gid = umgr.get_group_info_by_name(group)["gid"]
+        os.chown(path, uid, gid)
 
+        logger.log(logging.INFO, logger.LOG_TYPE_CONFIG,
+                   "Share directory (%s) owner is changed to (%s:%s)"
+                   " by user(%s)" %
+                   (path, user, group, operator))
 
+    def mod_share_mode(self, name, mode, operator="unknown"):
+        if not self.is_available():
+            raise StorLeverError("File system is unavailable", 400)
+        path = os.path.join(self.fs_conf["mount_point"], name)
+        if not os.path.exists(path):
+            raise StorLeverError("Share directory not found", 404)
+        os.chmode(path, mode)
 
-    def mod_share_owner(self, user, group):
-        pass
-
-    def mod_share_mode(self, mode):
-        pass
-
+        logger.log(logging.INFO, logger.LOG_TYPE_CONFIG,
+                   "Share directory (%s) mode is changed to 0%o"
+                   " by user(%s)" %
+                   (path, mode, operator))
 
 
 
