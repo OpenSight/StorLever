0b7ff37b1f3124ac197c42e1cc358c1bc770978c
missyoujojo <missyoujojo@163.com>
2014-03-13 00:46:09 +0800

    test view

---
+++ storlever/static/dist/scripts/eb3a0575.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("angularKickstartApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("angularKickstartApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
