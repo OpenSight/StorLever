9045ed3429caeea16c78ea047d9c0fba0dba1ef2
van <optvan@gmail.com>
2014-03-15 00:06:55 +0800

    URL 定义

--- storlever/rest/network.py
+++ storlever/rest/network.py
@@ -2,6 +2,7 @@ from storlever.rest.common import (get_view, post_view,
                                    put_view, delete_view)
 from storlever.lib.exception import StorLeverError
 from pyramid.response import FileResponse
+from storlever.mngr.network import ifmgr
 
 
 def includeme(config):
@@ -14,15 +15,26 @@ def includeme(config):
     # POST:   start/stop interface
     # PUT:    modify interface parameter
     # DELETE: delete bound interface
-    config.add_route('network_interface', '/network/{interface}')
+    config.add_route('port_list', '/network/port_list')
+	#config.add_route('single_list', '/network/single_list')
+	#config.add_route('single_port', '/network/single_list/{port_name}')
+	#config.add_route('bond_list', '/network/bond_list')
+	#config.add_route('bond_port', '/network/bond_list/{port_name}')
+	
+	
+    #config.add_route('network_interface', '/network/{interface}')
     # test purpose
     config.add_route('download_conf', '/download')
 
 
-@get_view(route_name='network')
+@get_view(route_name='port_list')
 def network_get(request):
-    return {'eth0': {'ip': '192.168.1.22', 'gateway': '192.168.1.1'}}
-
+	eth_face = ifmgr.if_mgr()
+	eth_list = eth_face.interface_name_list()
+	eth_list_dict = []
+	for port in eth_list:
+		eth_list_dict.append(port)
+	return eth_list_dict
 
 @post_view(route_name='network')
 def network_post(request):
