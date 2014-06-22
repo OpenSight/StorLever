f34e552c9af41459fa1de65128bd805cd33032e5
hyt <hyt1981@gmail.com>
2014-06-22 23:36:14 +0800

    LVM: more improvement

--- storlever/mngr/block/lvm/lvm.py
+++ storlever/mngr/block/lvm/lvm.py
@@ -434,6 +434,10 @@ class _PV(object):
     def delete(self):
         self._vg.remove_pv(self.name)
 
+    def resize(self, size):
+        if lvm_pv_resize(self._hdlr, size) != 0:
+            self._vg.raise_from_error('Failed to resize PV {0} to size {}'.format(self.name, size))
+
 
 class LVMManager(object):
     def __init__(self):
@@ -592,8 +596,47 @@ class PV(object):
         with _LVM() as _lvm:
             _lvm.remove_pv(self.dev_file)
 
+    def resize(self, size):
+        with _LVM() as _lvm:
+            with _VG(_lvm, self.vg.name) as _vg:
+                with _PV(_vg, name=self.dev_file) as _pv:
+                    _pv.resize(size)
+                    self.size = _pv.get_size()
+                    self.free = _pv.get_free_size()
+
+    def move(self, device):
+        pass
+
 
 class LV(object):
+
+    TYPE_ABBR = {
+        'm': 'mirrored',
+        'M': 'mirrored without initial sync',
+        'o': 'origin',
+        'O': 'origin with merging snapshot',
+        'r': 'raid',
+        'R': 'raid without initial sync',
+        's': 'snapshot',
+        'S': 'merging snapshot',
+        'p': 'pvmove',
+        'v': 'virtual',
+        'i': 'mirror or raid image',
+        'I': 'mirror or raid image out-of-sync',
+        'l': 'mirror log device',
+        'c': 'under conversion',
+        'V': 'thin volume',
+        't': 'thin pool',
+        'T': 'thin pool data',
+        'e': 'raid or thin pool metadata',
+    }
+
+    PERMISSION_ABBR = {
+        'w': 'writable',
+        'r': 'read-only',
+        'R': 'read-only activation of non-read-only volume'
+    }
+
     def __init__(self, vg, name=None, _lv=None):
         self.lock = vg.lock
         self.vg = vg
@@ -616,6 +659,9 @@ class LV(object):
             self.attr = _lv.get_attr()
         else:
             raise StorLeverError('No LV name given')
+        self.type = self.TYPE_ABBR.get(self.attr[0], 'unknown')
+        self.permission = self.PERMISSION_ABBR.get(self.attr[1], 'unknown')
+        self.partial = True if self.attr[8] == 'p' else False
 
     def resize(self, size):
         with self.lock:
