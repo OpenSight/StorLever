8efcee4e18ebf5f2cfb88a400adfc27594eee6ba
Don Quixote <song.dxs@gmail.com>
2014-10-13 00:02:26 +0800

    show cpu list

--- storlever/static/js/partials/system-info.js
+++ storlever/static/js/partials/system-info.js
@@ -1,10 +1,12 @@
 (function(){
     controllers.controller('SystemInfo', ['$scope', '$http', function($scope, $http){
         $scope.selectOverview = function(){
-            $http.get("/storlever/api/v1/system/localhost")
-                .success(function(response) {
-                    $scope.localhost = response;
-                });
+            $http.get("/storlever/api/v1/system/localhost").success(function(response) {
+                $scope.localhost = response;
+            });
+            $http.get("/storlever/api/v1/system/cpu_list").success(function(response) {
+                $scope.cpulist = response;
+            });
         };
         $scope.selectConfig = function(){
             alert('selectConfig');
