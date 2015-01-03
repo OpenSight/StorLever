a97cd04611ea12ef4c8f08671409ea78aac70c34
root <root@iZ23ic3ug72Z.(none)>
2015-01-03 11:09:14 +0800

    show cpu info

--- storlever/static/js/filters.js
+++ storlever/static/js/filters.js
@@ -1,10 +1,25 @@
-'use strict';
+(function(){
+  'use strict';
 
 /* Filters */
 
-angular.module('app.filters', []).
-  filter('interpolate', ['version', function(version) {
+  angular.module('app.filters', []).filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     };
+  }]).filter('KB', [function() {
+    var step = 1024;
+    var unit = ·['KB', 'MB', 'GB', 'TB'];
+    return function(capacity, bit) {
+      for (var i = 0, l = unit.length; i < l; i++){
+        capacity = capacity / step;
+        if (capacity < 0){
+          break;
+        }
+      }
+      var tmp = Math.pow(10, bit);
+      capacity = Math.round(capacity * step * tmp) / tmp;
+      return String(text).replace(/\%VERSION\%/mg, version);
+    };
   }]);
+})();
